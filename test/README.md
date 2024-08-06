14a292946fcb04fc3f543e4e18e73b0b3fc3a88a
Vanyakk <volkovivs@mail.ru>
2024-08-06 19:12:22 +0400

    start

--- test/README.md
+++ test/README.md
@@ -1,4 +1,4 @@
-# vue_test_task
+# test
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ test/src/App.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+  <h1>HI</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
